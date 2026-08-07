d4f909336a072ca828b6143b61415b30e3135cfb
McCapy <austinscott1035@gmail.com>
2026-08-07 08:09:04 -0500

    Fixed something

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,8 @@ import org.refined.AsyncStream;
 void main() {
     // new error handling (allows for defaults, lowering the need for ifNull catches)
     // error catching is **NO LONGER REQUIRED** originally it would ruin your code,
-    // forcing you to both catch implicitly, but also do an ifNull check.
+    // forcing you to both catch implicitly, but also do a ifNull check, this is
+    // no longer needed, as you can offer a value to replace the null on error.
     AsyncStream<Integer> stream =
         new AsyncStream<>(1,2,3,4,5)
             .map((item) -> {
@@ -13,16 +14,17 @@ void main() {
                 return item + 1; // so the results of this get disregarded.
             })
             .catchError(() -> new Integer[]{10,9,8,7,6}) // this just offers a value on error
-            .catchError((err) -> {
+            .catchError((_) -> {
                 // do smth w/ error
             })
             .catchError() // cancels on error
-            .catchError(err -> {
+            .catchError(_ -> {
                 // do smth w/ error
                 return new Integer[]{10,9,8,7,6};
             }) // offers a value and does smth with the error
             .catchError(() -> {
                 System.out.println("Smth");
+                System.out.println("Smth else");
             })
             .start();
     System.out.println("Result joined from child thread: " + Arrays.toString(stream.join()));
