5055377e6ea7b6eaeff2a51ff0d36031ba27f0ec
McCapy <austinscott1035@gmail.com>
2026-09-03 13:30:24 -0500

    todo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,4 +16,14 @@ void main() {
             .collect(Integer.class,List.of("example_id"))
             .toList();
     System.out.println(result);
+    /*
+    make AsyncStages contain each other for compact execution, add error handling
+    cancellation handling, as well as completion handling be self contained inside
+    the tasknodes to prevent the blowup of the streamscope, from there im going to
+    merge the streamscope into the AsynchronousStream so i dont have to worry about
+    the existence of the third file, and then maybe do some extra stuff to the
+    AsyncStage like converting it into fields rather, rather then doing list caches
+    This will lower the amount of param lists that i have to mess with (which just..
+    makes it more convenient to make asyncstages/tasknodes)
+     */
 }
