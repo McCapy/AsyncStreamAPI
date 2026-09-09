7c8167c0cfdb42966dcab5bafd899e4fdc92c252
McCapy <austinscott1035@gmail.com>
2026-09-09 13:05:02 -0500

    fixed some stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,6 @@ import org.refined.AsyncStream;
 void main() {
     AsyncStream<Integer> stream =
         new AsyncStream<>(1,2,3,4,5,6,7,8,9,10)
-            .fork(items ->
-                new AsyncStream<>(items)
-                    .map(item -> item * 2)
-            )
-            .collect(0,Integer.class)
             .start();
     System.out.println(stream.toList());
 
--- src/main/java/org/refined/AsyncStage.java
+++ src/main/java/org/refined/AsyncStage.java
@@ -13,18 +13,16 @@ public abstract class AsyncStage<I,O> {
     public abstract @NotNull List<?> compute(@NotNull AsynchronousStream<?> scope, @UnknownNullability List<?> items) throws RuntimeException;
 
     protected boolean catching = false;
-    protected boolean failed = false;
     protected AsyncStage<?,?> next;
 
     public final List<?> start(AsynchronousStream<?> stream, List<?> items) {
         try {
-            if (failed) return find().advance(stream, items);
             try {
                 return advance(stream, compute(stream, items));
             } catch (RuntimeException e) {
-                if (next != null) next.failed = true;
                 if (catching) return advance(stream, ((YieldStage<?>) find()).fn.apply(e));
                 new MissingSyntaxException("An error that wasn't caught was thrown.",e).printStackTrace();
+                stream.cancel();
                 return AsynchronousStream.EMPTY;
             }
         } catch (MissingSyntaxException e) {
@@ -36,10 +34,7 @@ public abstract class AsyncStage<I,O> {
     private AsyncStage<?,?> find() throws MissingSyntaxException {
         try {
             AsyncStage<?, ?> current = next;
-            while (!(current instanceof YieldStage<?>)) {
-                System.out.println("Next");
-                current = current.next;
-            }
+            while (!(current instanceof YieldStage<?>)) current = current.next;
             return current;
         } catch (NullPointerException e) {
             throw new MissingSyntaxException("No accompanying Yield to the given Guard.",e);
